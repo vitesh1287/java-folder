ece6b3951db88a45fec206e30b2aff5eab5ed71a
vitesh1287 <viteshguptavarun@gmail.com>
2025-06-30 18:34:24 +0530

    message

---
+++ ArraySum.java
@@ -0,0 +1,13 @@
+public class ArraySum {
+    public static void main(String[] args){
+        int[] arr ={10,20,30,40,50};
+        int sum =0;
+        for( int i=0; i < arr.length; i++){
+            sum+=arr[i];
+
+        }
+        System.out.println("sum of array elements = " + sum); 
+
+    }
+
+}
---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo{
+    String name;
+    String des;
+    public static void main(String[]argu){
+        Demo d=new Demo(); // reference varible
+        d.name = "vitesh";
+        d. des = "assitant prof";
+        System.out.println(d.name+"  "+d.des);
+    }
+
+    }
