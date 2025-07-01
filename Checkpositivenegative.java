99f5462a6b73162c1181acf32bf48cee4d9e22db
vitesh1287 <viteshguptavarun@gmail.com>
2025-07-01 16:31:10 +0530

    checkpositivenegative

---
+++ Checkpositivenegative.java
@@ -0,0 +1,12 @@
+public class Checkpositivenegative {
+    public static void main(String[] args){
+        int number = -7;
+        if( number > 0){
+            System.out.println("the number is positive");
+        }else{
+            System.out.println("the number is negative");
+
+        }
+
+        }
+    }
