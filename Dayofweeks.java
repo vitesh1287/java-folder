1eb6c7c15a2c5d61b3229055736e056c8cdf5c6a
vitesh1287 <viteshguptavarun@gmail.com>
2025-07-01 16:58:24 +0530

    dayofweeks

---
+++ Dayofweeks.java
@@ -0,0 +1,27 @@
+public class Dayofweeks{
+    public static void main(String[] args){
+        int day=5;
+        switch(day){
+            case 1:
+            System.out.println("monday");
+            break;
+            case 2:
+            System.out.println("tuesday");
+            break;
+            case 3:
+            System.out.println("wednesday");
+            break;
+            case 4:
+            System.out.println("thursday");
+            break;
+            case 5:
+            System.out.println("friday");
+            break;
+            case 6:
+            System.out.println("saturday");
+            break;
+            default:
+            System.out.println("invalid day");
+        }
+    }
+}
