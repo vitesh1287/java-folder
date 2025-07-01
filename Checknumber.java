09e4cbba994a187a09dce1db0b55b5f261033596
vitesh1287 <viteshguptavarun@gmail.com>
2025-07-01 16:17:03 +0530

    check.java

---
+++ Checknumber.java
@@ -0,0 +1,8 @@
+public class Checknumber {
+    public static void main (String[] args){
+        int number = 10;
+        if(number>5){
+            System.out.println("the number is greter than 5");
+        }
+    }
+}
