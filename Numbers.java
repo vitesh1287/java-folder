cfdf1f5412f81c75781601c6dc0acf911093676e
vitesh1287 <viteshguptavarun@gmail.com>
2025-07-01 19:28:39 +0530

    Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,11 @@
+public class Numbers {
+    public static void main(String[] args){
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i=i+1;
+
+        }
+
+    }
+}
