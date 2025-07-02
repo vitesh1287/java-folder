6b84176ac9a9800f08a4f70febcbe00d8f053b47
vitesh1287 <viteshguptavarun@gmail.com>
2025-07-02 19:34:32 +0530

    object.java

---
+++ object.java
@@ -0,0 +1,14 @@
+public class object{
+    int a=10; String b="vitesh";
+    void show(){
+        System.out.println(a+"  "+b);
+    }
+}
+class Test
+{
+    public static void main(String[] args){
+    object r = new object();
+    r.show();
+    }
+}
+
