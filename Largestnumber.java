e92a5e83a37b1fbd8375e5d92717ae0f06aadb0a
vitesh1287 <viteshguptavarun@gmail.com>
2025-07-03 19:10:46 +0530

    largestnumber.java

---
+++ Largestnumber.java
@@ -0,0 +1,15 @@
+public class Largestnumber{
+    public static void main(String[] args){
+        int a = 50, b = 60, c = 30; 
+        if(a>=b && a>=c){
+            System.out.println("the largest number is: " + a);
+        }else if(b>=a && b>=c){
+            System.out.println("the largest number is: " + b);
+        }else{
+            System.out.println("the largest number is: " + c);
+        }
+    }
+}
+
+        
+    
