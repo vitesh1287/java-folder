fd621a0daf1835e685de81b069679df9c3304bd7
vitesh1287 <viteshguptavarun@gmail.com>
2025-07-03 17:39:26 +0530

    Swapnumbers.java

---
+++ Swapnumbers.java
@@ -0,0 +1,17 @@
+public class  Swapnumbers{
+    public static void main(String[] args){
+        int a=5;
+        int b=10;
+        System.out.println("Before swap:");
+        System.out.println("a =" +a);
+        System.out.println("b =" +b);
+    int temp = a;
+    a = b;
+    b = temp;
+
+    System.out.println("After swap:");
+    System.out.println("a =" +a);
+    System.out.println("b =" +b);
+    }
+}
+    
